ebeeb2dfdde2ff3126148294d7ef9d7f39db4ac9
Bryan0820 <bryanli200282@gmail.com>
2021-11-29 17:21:57 -0500

    storing the stock data

--- tradebot.py
+++ tradebot.py
@@ -2,6 +2,7 @@ import alpaca_trade_api as tradeapi
 import numpy as np
 import time
 
+
 domain = "https://paper-api.alpaca.markets"
 Key_ID = "PKZJ2IUHPR22IWQBX83D"
 SEC_Key = "du6vf2uoCvDlO3Re9uLPUtk0JfmrGm8KaTMuvisl"
@@ -18,38 +19,42 @@ print('The market is {}'.format('open.' if clock.is_open else 'closed.'))
 account = api.get_account()
 positions = api.list_positions() # listing all the current position
 
-print(account.status)
+#print(account.status)
 print("available positions",positions)
 
 
 # available buying power
 print(f'${account.buying_power} is available as buying power.')
 
-#Getting Data for the stock
-barset = api.get_barset('SPY', 'day', limit=5) # using the daily candle
+while True:
+    print("")
+    print("checking price")
+
+    #Getting Data for the stock
+    barset = api.get_barset('SPY', 'minute', limit=30) # using the last 5 trading days 30 min candlesticks
+    bars = barset['SPY']
+
+    week_open = bars[-1].o
+    #print("open",week_open)
+    week_close = bars[-1].c
+    #print("close",week_close)
+
+    percent_change = (week_close - week_open) / week_open * 100
+    #print(f'SPY moved {percent_change}% over the last 5 days')
 
 
-bars = barset['SPY']
-week_open = bars[0].o
-print("open",week_open)
-week_close = bars[0].c
-print("close",week_close)
-percent_change = (week_close - week_open) / week_open * 100
-print(f'SPY moved {percent_change}% over the last 5 days')
+    #storing the data
 
+    data = []
+    for bar in barset["SPY"]:
+        data.append(bar.c)  # getting the closing data of the stock
 
+    data = np.array(data,dtype=np.float64) # convert the the list into numpy
+    Ma = np.mean(data) # moving average
+    last_price = data[-1] # the last closing price
 
-
-
-
-
-
-
-
-
-
-
-
+    print("Moving Average: " + str(Ma))
+    print("Last Price: " + str(last_price))
 
 
 
@@ -59,7 +64,7 @@ print(f'SPY moved {percent_change}% over the last 5 days')
 
 #Buy a stock in market price
 """api.submit_order(
-    symbol = "SPY", # stock ticker symbol in capital
+    symbol = "SPY", # stock ticker symbol in capital case
     qty = 1, 
     side = "buy",
     type = "market",
@@ -67,7 +72,7 @@ print(f'SPY moved {percent_change}% over the last 5 days')
 )
 #sell a stock in market price 
 api.submit_order(
-    symbol = "SPY", # stock ticker symbol in capital
+    symbol = "SPY", # stock ticker symbol in capital case
     qty = 1, 
     side = "sell",
     type = "market",
