97b10f1ff1965415be3666cd62c2231643854505
Bryan0820 <bryanli200282@gmail.com>
2021-12-02 00:35:24 -0500

    implementation of the strategy

--- tradebot.py
+++ tradebot.py
@@ -51,6 +51,10 @@ while True:
     last_price = data5[-1] # getting the last closing price
 
     #making the 9 nad 21 moving averages cited:https://www.delftstack.com/howto/python/moving-average-python/#:~:text=Use%20the%20pandas%20Module%20to%20Calculate%20the%20Moving%20Average,-Time%20series%20data&text=We%20first%20convert%20the%20numpy,using%20the%20mean()%20function.
+    
+    # strategy: 9 and 21 ema crossover, when 9 ema is greater than 21 ema we buy. and when 9 ema is less than 21 we sell 
+
+
 
     # the 9 moving average
     data9 = []
@@ -59,8 +63,8 @@ while True:
 
     data9 = np.array(data9,dtype=np.float64) # convert the the list into numpy
 
-    Ma_9 = pd.Series(data9)
-    last_MA_9 = Ma_9.mean()
+    Ma_9 = pd.Series(data9) # a list of all the data 
+    last_MA_9 = Ma_9.mean() # The mean of the list
     print("the 9 moving average", last_MA_9)
 
     # the 21 moving average
@@ -70,36 +74,42 @@ while True:
 
     data21 = np.array(data21,dtype=np.float64) # convert the the list into numpy
 
-    Ma_21 = pd.Series(data21)
-    last_MA_21 = Ma_21.mean()
+    Ma_21 = pd.Series(data21) # a list of all the data 
+    last_MA_21 = Ma_21.mean() # the mean of the list
     print("the 21 moving average", last_MA_21)
 
 
 
-    print("Last Price: " + str(last_price)) # on the five minute chart 
+    print("Last Price: " + str(last_price)) # last price on the five minute chart 
 
     
     print(data5)
 
+    # implementation of the strategy
+    if last_MA_9 > last_MA_21:
+        #Buy a stock in market price
+        print("Buy 1 share")
+        api.submit_order(
+        symbol = "SPY", # stock ticker symbol in capital case
+        qty = 1, 
+        side = "buy",
+        type = "market",
+        time_in_force = "gtc" # good till cancel 
+    )
+    
+    elif last_MA_9 < last_MA_21: 
+        #sell a stock in market price
+        print("Sell 1 share")
+        api.submit_order(
+        symbol = "SPY", # stock ticker symbol in capital case
+        qty = 1, 
+        side = "sell",
+        type = "market",
+        time_in_force = "gtc" # good till cancel 
+    )
+    
+            
 
 
 
 
-
-
-#Buy a stock in market price
-"""api.submit_order(
-    symbol = "SPY", # stock ticker symbol in capital case
-    qty = 1, 
-    side = "buy",
-    type = "market",
-    time_in_force = "gtc" # good till cancel 
-)
-#sell a stock in market price 
-api.submit_order(
-    symbol = "SPY", # stock ticker symbol in capital case
-    qty = 1, 
-    side = "sell",
-    type = "market",
-    time_in_force = "gtc" # good till cancel 
-)"""
